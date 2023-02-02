78d04547d42f51b7e33e760fbb4e33db1a677eda
alexfonteles <allsce@hotmail.com>
2023-02-01 21:26:55 -0300

    Sobre construida

--- React/Projetos/sorveteria-artesanal/src/components/Capa/index.js
+++ React/Projetos/sorveteria-artesanal/src/components/Capa/index.js
@@ -3,7 +3,7 @@ import './Capa.css';
 function Capa() {
   return (
     <div className="capa">
-        <img src='assets/banner-home.png' alt='logo'/>
+        <img src='assets/banner-home.png' alt='banner-capa'/>
         <div className='box-capa'>
           <h3 id='texto'>SORVETE ARTESANAL</h3>
         </div>
